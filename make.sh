9619bfe6146919484af9252f10f947a6a5307f7c
albfan <albertofanjul@gmail.com>
2015-10-31 19:05:35 +0100

    remove deprecated BSD_SOURCE

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CC_FLAGS="-O3 -Wall -std=c99 -D_BSD_SOURCE -D_XOPEN_SOURCE=500"
+CC_FLAGS="-O3 -Wall -std=c99 -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=500"
 echo "compiling 32-bit version"
 clang -m32 $CC_FLAGS -o fsnotifier main.c inotify.c util.c
 if [ $? -eq 0 ] ; then
